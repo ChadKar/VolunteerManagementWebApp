bfa48bbbc2cc6042ff03ea5809e3e99da437f69b
shawbeva <shawbeva@gs1803.AD.ECS.VUW.AC.NZ>
2019-01-17 10:36:55 +1300

    Adding project to date
    Still messy and needs sorting but everyone can see the same thing now.

---
+++ VolunteerManagementWebApp/login_script.js
@@ -0,0 +1,42 @@
+  var config = {
+    apiKey: "AIzaSyAefGkmEiYGdjYTspccWr2zA_ilYOwfQxU",
+    authDomain: "volunteer-management-20853.firebaseapp.com",
+    databaseURL: "https://volunteer-management-20853.firebaseio.com",
+    projectId: "volunteer-management-20853",
+    storageBucket: "volunteer-management-20853.appspot.com",
+    messagingSenderId: "574062360222"
+ };
+  
+firebase.initializeApp(config);
+var database = firebase.database();
+        
+
+function getUserData() {
+    "use strict";
+    event.preventDefault();
+    // get email string into username
+    var enteredEmail = document.getElementById("emailEntry").value, enteredPassword = document.getElementById("passwordEntry").value, user = enteredEmail.split("@"), ending = user[1];
+    var myStr = ending;
+    var newStr = myStr.replace(".", "~");
+    var username = user[0] + "@" + newStr;
+    console.log("This is true username: " + username);
+    
+    database.ref('/manager/' + username).once("value").then(function (data) {
+        if (data.val() === null) {
+            console.log("No Account exists with this email address. Please register.");
+            alert("No Account exists with this email address. Please register");
+        } else {
+
+            var realEm = (data.val().email), realPass = (data.val().password);
+            if ((realEm === enteredEmail) && (realPass === enteredPassword) && (data.val().status) === "active") {
+                localStorage.setItem("user_email", data.val().email);
+                localStorage.setItem("user_name", data.val().name);
+                //localStorage.setItem("", );
+                window.location = "chooseAction.html";
+            } else {
+                console.log("Please try again. Invalid email and password combination.");
+            	alert("Please try again. Invalid email and password combination.");
+            }
+        }
+    });
+}
---
+++ VolunteerManagementWebApp/register_script.js
@@ -0,0 +1,140 @@
+var config = {
+    apiKey: "AIzaSyCPvM0PidSAxE5ev5iNGl7qfbVj5vK4zv8",
+    authDomain: "stackover-burger.firebaseapp.com",
+    databaseURL: "https://stackover-burger.firebaseio.com",
+    projectId: "stackover-burger",
+    storageBucket: "stackover-burger.appspot.com",
+    messagingSenderId: "359154422713"
+};
+        
+firebase.initializeApp(config);
+var database = firebase.database();
+  
+function checkFields() {
+    var colourbackground = "#d5cdf3";
+    console.log("Clicked register");
+    event.preventDefault();
+    var okay = true;
+    var xxx = document.getElementById("firstNameEntry").value.trim();
+    if(document.getElementById("firstNameEntry").value === "") {
+        document.getElementById("firstNameEntry").style.background = colourbackground;
+        document.getElementById("regFeedback").innerHTML = "Please enter all fields";
+        okay = false;
+    }
+    if(document.getElementById("lastNameEntry").value === "") {
+        document.getElementById("lastNameEntry").style.background = colourbackground;
+         okay = false;
+    }
+    if(document.getElementById("regemailEntry").value === "") {
+        document.getElementById("regemailEntry").style.background = colourbackground;
+         okay = false;
+    }    
+    if(document.getElementById("dobEntry").value === "") {
+        document.getElementById("dobEntry").style.background = colourbackground;
+         okay = false;
+    }
+    if(document.getElementById("phoneEntry").value === "") {
+        document.getElementById("phoneEntry").style.background = colourbackground;
+         okay = false;
+    }
+    if(document.getElementById("passwordEntry").value === "") {
+        document.getElementById("passwordEntry").style.background = colourbackground;
+         okay = false;
+    }
+    if(document.getElementById("passwordEntry2").value === "") {
+        document.getElementById("passwordEntry2").style.background = colourbackground;
+         okay = false;
+    }
+    console.log("Print:" + okay);
+    if(okay === true) {
+        console.log("parameters called");
+        checkPassowrdEquals();
+    }
+   
+}
+
+function checkPassowrdEquals() {
+    var check = false;
+    var pass1 = document.getElementById("passwordEntry").value.trim();
+    var pass2 = document.getElementById("passwordEntry2").value.trim();
+    if(pass1 === pass2) {
+        check = true;
+        console.log("passwords equal");
+        getParameters();
+    }
+}
+
+function getParameters() {
+    console.log("getParameters called");
+    var firstname = document.getElementById("firstNameEntry").value.trim();
+    var lastname = document.getElementById("lastNameEntry").value.trim();
+    var email = document.getElementById("regemailEntry").value.trim();
+    var dob = document.getElementById("dobEntry").value.trim();
+    var phone = document.getElementById("phoneEntry").value.trim();
+    var password = document.getElementById("passwordEntry").value.trim();
+    var password2 = document.getElementById("passwordEntry2").value.trim()
+    console.log(firstname + "," + lastname + ", " + email + ", " + dob + ", " + phone + ", " + password);
+    checkUserExists();
+}
+
+function addUser(em, birth, usern, namE, pass, ph) {
+  firebase.database().ref('/customer/' + usern).set({
+    dob: birth,
+    email: em,
+    name: namE,
+    password: pass,
+    phone: ph
+  });
+    console.log("User Added");
+}
+
+
+function checkUserExists() {
+    console.log("CheckUserExists called");
+    
+    var firstname = document.getElementById("firstNameEntry").value.trim();
+    var lastname = document.getElementById("lastNameEntry").value.trim();
+    var dob = document.getElementById("dobEntry").value.trim();
+    var phone = document.getElementById("phoneEntry").value.trim();
+    var password = document.getElementById("passwordEntry").value.trim();
+    var Name = firstname + " " + lastname;
+    var enteredEmail = document.getElementById("regemailEntry").value, user = enteredEmail.split("@"), ending = user[1];
+    var myStr = ending;
+    var newStr = myStr.replace(".", "~");
+    var username = user[0] + "@" + newStr;
+    console.log("This is true username: " + username);
+    
+     database.ref('/customer/' + username).once("value").then(function (data) {
+        if (data.val() === null) {
+            console.log("No Account exists with this email address");
+            addUser(enteredEmail, dob, username, Name, password, phone);
+            window.location = "login.html";
+        }
+         else {
+             console.log("Account Exists");
+             alert("An account already exists associated with this email address");
+            //document.getElementById("loginFeedback").innerHTML = "An account already exists associated with this email address";
+         }
+        })
+}
+    
+
+function getUserData() {
+    "use strict";
+    event.preventDefault();
+    // get email string into username
+    var enteredEmail = document.getElementById("emailEntry").value, enteredPassword = document.getElementById("passwordEntry").value, user = enteredEmail.split("@"), username = user[0];
+    database.ref('/customer/' + username).once("value").then(function (data) {
+        if (data.val() === null) {
+            console.log("No Account exists with this email address");
+            document.getElementById("loginFeedback").innerHTML = "No Account exists with this email address";
+        } else {
+            var realEm = (data.val().email), realPass = (data.val().password);
+            if ((realEm === enteredEmail) && (realPass === enteredPassword)) {
+                window.location = "register.html";
+            } else {
+                window.document.getElementById("loginFeedback").innerHTML = "Something didnt match";
+            }
+        }
+    });
+}
