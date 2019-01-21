b945299d9b6ea209af9eceaf453967f7a06b077c
paultana <paultana@ecs.vuw.ac.nz>
2019-01-22 02:21:31 +1300

    work in progress on volenteer avalibility page.

--- VolunteerManagementWebApp/available.js
+++ VolunteerManagementWebApp/available.js
@@ -9,6 +9,47 @@
 firebase.initializeApp(config);
 var database = firebase.database();
 
+function getAvailability(){
+	 alert("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");	
+
+"use strict";
+event.preventDefault();
+
+var day = document.getElementById("dateEntry").value ;
+alert("--BB---"+day);	
+
+console.log("searching volunteer availibility for :" + day);
+alert("---1---");	
+
+//database.ref('/volunteer/volunteer1@gmail~com').once("value").then(function (data) {
+//database.ref('/availability/'+day).once("value").then(function (data) {
+database.ref('/availability/'+day).once("value").then(function (data) {
+	
+	console.log("Volunteer available  --" + data.val());
+	alert("Volunteer available  --"+data.val());
+//	alert("available --"+data.val().available.friday);
+//	data.forEach(function(childData){
+//		var key = childData.ket;
+//	})
+   if (data.val() === null) {
+       console.log("Data could not be found");
+       alert("---2  A---");
+   } else {
+   	alert("---2  B ---");
+       var emailId = (data.val().emailId) ;
+       if (emailId != null) {
+    	   alert("---2  C ---");
+           localStorage.setItem("emailId", emailId);
+           //localStorage.setItem("", );
+          window.location = "available.html";
+       } 
+   }
+   localStorage.setItem("user_name", "Tanaya");
+   localStorage.setItem("user_email", "tanaya@gmail.com");
+});
+
+}
+
 
 function setButtons(){
     document.getElementById("add").addEventListener("click", function(){
